99b71e8cd220546da1ea03c309c52c1716015cbf
PRAKASHATHUL <athulprakash442004@gmail.com>
2025-07-10 14:08:45 +0530

    for loop and range introduction

---
+++ for_loop_sample.py
@@ -0,0 +1,30 @@
+my_list=[23,45,63,78]
+for list in my_list:
+#for list in enumerate (my_list): #enumerate 
+    print(list,end=" ")
+print("")
+ 
+my_tuple=(23,43,12,76)
+for tuple in my_tuple:
+    print(tuple,end=":")
+print("")
+ 
+    
+my_dict={"name":"nadhil","dino":67,78:"forlan",34:32}
+#for dict in my_dict:
+    #print(dict,end=":")
+    #print(my_dict[dict])
+#print("") #to make a break in the output
+print(my_dict)    
+for dict in my_dict.items():
+    print(dict)
+print("")
+
+
+my_set={12,45,78}
+for set in my_set:
+    print(set,end="-")
+
+
+    
+    
---
+++ range_prgm.py
@@ -0,0 +1,15 @@
+"""num=int(input("Enter the number:"))
+for k in range(1,11):
+    print(f"{k}*{num}={k*num}")
+print("")
+"""
+    
+"""name=input("Enter name :")
+for n in range(8):
+    print(name)
+    """
+
+num=int(input("Enter the number:"))
+for v in range(1,num):
+    if v%2==0:
+        print(v)
