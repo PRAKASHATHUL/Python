e36dcdaa65c3a44ce80bc0da40f4653a69b1b646
PRAKASHATHUL <athulprakash442004@gmail.com>
2025-07-16 14:21:16 +0530

    Some function programs

---
+++ function.py
@@ -0,0 +1,39 @@
+#Function is a reusable code.
+#Function is a block of code which is executed when it is called.
+#def fun(): - bracketinta akath kodukanen Parameters en parayum.
+#    print("Hello")
+#fun() - brackinta akath kodukanen Arguments en parayum,
+
+"""def fun():
+    print("Good Bye Zaman")
+    print("Alvaro carreras")
+fun()"""
+
+#def fun():
+#   pass - pass kodutha intentaction error kanikula, ee function kanikula
+#fun()
+
+"""def name(num):
+    f=count=1
+    while count<=num:
+        f=f*count
+        count+=1
+    print(f)
+name(5)
+
+def is_even(num):
+    if num%2==0:
+        print(num,"is even")
+    else:
+        print(num,"is odd")
+num=int(input("Enter the no:"))
+is_even(num)"""
+
+def is_positive(num):
+    if num>=0:
+        print(num,"is positive")
+    else:
+        print("Please enter a positive number")
+num=int(input("Enter the no "))
+is_positive(num)
+        
